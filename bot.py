d051f5d05122f9d9c2421350a1f4e5ef7efb2062
Dhanush20038 <115239105+Dhanush20038@users.noreply.github.com>
2022-11-29 12:43:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/TheInsomniacsClub")
         self.LOGGER(__name__).info(f""" \n\n       
 TheInsomniacsClub
                                           """)
